f34d38f2249722d0d29caa7d30e10e601fed5987
huhaitao <huhaitao@100tal.com>
2018-01-24 18:35:16 +0800

    add test code

--- test/conv.js
+++ test/conv.js
@@ -1,10 +1,27 @@
 const path = require('path')
 const conv = require('../index')
 
-conv(path.resolve(__dirname, 'pp/test.pptx'), path.resolve(__dirname, 'out/pp-test.pdf'))
-.then(code => {
-  console.log('ok', code)
+const ppSource = path.resolve(__dirname, 'pp/test.pptx')
+
+async function main () {
+  await conv(ppSource, path.resolve(__dirname, 'out/pp-test.pdf'))
+  console.log('pdf ok')
+  await conv(ppSource, path.resolve(__dirname, 'out/pp-test-png'), 'PNG')
+  console.log('png ok')
+  await conv(ppSource, path.resolve(__dirname, 'out/pp-test-jpg'), 'jpg')
+  console.log('jpg ok')
+  await conv(ppSource, path.resolve(__dirname, 'out/pp-test-xps.xps'), 'xPs')
+  console.log('xps ok')
+  await conv(ppSource, path.resolve(__dirname, 'out/pp-test-bmp.bmp'))
+  console.log('bmp ok')
+  await conv(ppSource, path.resolve(__dirname, 'out/pp-test-gif'), 'Gif')
+  console.log('gif ok')
+}
+
+main()
+.then(() => {
+  console.log('all ok')
 })
-.catch(code => {
-  console.error('error', code)
+.catch(err => {
+  console.error(err)
 })
